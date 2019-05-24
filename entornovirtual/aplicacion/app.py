4e327b58e9c5dc1213f6a935ae85b4bd9a7219e5
Jaime Abad Afonso <jaime.abad.afonso@gmail.com>
2019-05-24 14:57:33 +0200

    more

--- entornovirtual/aplicacion/app.py
+++ entornovirtual/aplicacion/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template # Constructor
+from flask import Flask, render_template, redirect, url_for, request # Constructor
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from aplicacion import config
+from aplicacion.forms import FormCategoria, FormArticulo
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__) # Aplicación WSGI
 app.config.from_object(config) # Cargamos las variables de configuración
@@ -9,6 +11,7 @@ Bootstrap(app)
 db = SQLAlchemy(app) # Objeto db con el constructor SQLAlchemy que representa la database
 
 from aplicacion.model import Articulos, Categorias # esta importación debe hacerse después del objeto db
+
 
 @app.route('/') # Decorador
 @app.route('/categoria/<id>')
@@ -27,12 +30,12 @@ def init(id='0'):
 @app.route('/categorias')
 def categorias():
 	allCategorias = Categorias.query.all()
-	return render_template("index.html", categorias=allCategorias)
+	return render_template("categorias.html", categorias=allCategorias)
 
 
 @app.route('/categorias/new', methods=["get", "post"])
 def categorias_new():
-	form = formCategoria(request.form)
+	form = FormCategoria(request.form)
 	if form.validate_on_submit():
 		cat = Categorias(nombre=form.nombre.data)
 		db.session.add(cat)
@@ -44,16 +47,22 @@ def categorias_new():
 
 @app.route('/articulos/new', methods=["get", "post"])
 def articulos_new():
-	form = formArticulo()
+	form = FormArticulo()
+
+	# utilizamos un generador para rellenar los choices
 	categorias = [ (c.id, c.nombre) for c in Categorias.query.all()[1:] ]
+	# Recorremos las categorias y las guardamos en una lista de tuplas para inicializar el CategoriaId
 	form.CategoriaId.choices = categorias
+
 	if form.validate_on_submit():
 		try:
-			f = form.photo.databasenombre_fichero = secure_filename(f.filename)
+			f = form.photo.data
+			nombre_fichero = secure_filename(f.filename)
 			f.save( app.root_path + "/static/media/" + nombre_fichero )
 		except:
 			nombre_fichero = ""
 		art = Articulos()
+		# cogemos los valores del formulario y rellenamos el objeto art
 		form.populate_obj(art)
 		art.image = nombre_fichero
 		db.session.add(art)
--- entornovirtual/aplicacion/forms.py
+++ entornovirtual/aplicacion/forms.py
@@ -1,1 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DecimalField, IntegerField, TextAreaField, SelectField
+from flask_wtf.file import FileField
+from wtforms.validators import Required
 # En este fichero se definen los modelos correspondientes a cada formulario 
+
+class FormCategoria(FlaskForm):
+	nombre = StringField( "Nombre:", validators=[Required("Tienes que introducir el dato")] )
+	submit = SubmitField( "Enviar" )
+		
+
+class FormArticulo(FlaskForm):
+	nombre 		= StringField( "Nombre:", validators=[Required("Tienes que introducir el dato")] )
+	precio 		= DecimalField( "Precio:", default=0, validators=[Required("Campo obligatorio")] )
+	IVA 		= IntegerField( "IVA:", default=21, validators=[Required("Campo obligatorio")] )
+	descripcion = TextAreaField("Descripción:")
+	photo 		= FileField("Selecciona imagen:")
+	stock 		= IntegerField( "Stock:", default=1, validators=[Required("Campo obligatorio")] )
+	CategoriaId = SelectField( "Categoría:", coerce=int ) # coerce indica el tipo de dato que va a devolver
+	submit 		= SubmitField("Enviar")
